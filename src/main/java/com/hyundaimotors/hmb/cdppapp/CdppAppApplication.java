9cfb10c2874f0a36551d69979c24e27a2c8e53a4
Anchors-jinseokj <jinseokj@anchors-biz.com>
2023-06-19 21:25:47 +0900

    clean push

---
+++ src/main/java/com/hyundaimotors/hmb/cdppapp/CdppAppApplication.java
@@ -0,0 +1,13 @@
+package com.hyundaimotors.hmb.cdppapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdppAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdppAppApplication.class, args);
+	}
+
+}
