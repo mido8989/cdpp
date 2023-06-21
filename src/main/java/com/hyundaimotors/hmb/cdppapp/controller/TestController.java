dabce45029fc61b8755e3ebaa2d8e19f61c941d0
Jinhyeok Choi <jhchoi@anchors-biz.com>
2023-06-21 17:24:13 +0900

    TestController merged

--- src/main/java/com/hyundaimotors/hmb/cdppapp/controller/TestController.java
+++ src/main/java/com/hyundaimotors/hmb/cdppapp/controller/TestController.java
@@ -2,7 +2,7 @@ package com.hyundaimotors.hmb.cdppapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,7 +20,7 @@ public class TestController {
     @Autowired
     private final TestServiceImpl testService;
 
-    @RequestMapping(value = "/insert", method = RequestMethod.POST)
+    @PostMapping(value = "/insert")
     public TestDto insertList(@RequestBody TestDto dto) throws Exception {
 
         TestDto responDto = testService.insertList(dto);
