e4eaf35a2ebcc75ec0ac2d0a441959d82231175c
Jinhyeok Choi <jhchoi@anchors-biz.com>
2023-07-20 20:40:02 +0900

    IF_HMB_ZICARD_CDPP_0001 작업 진행

---
+++ src/main/java/com/hyundaimotors/hmb/cdppapp/mapper/IFHMBZICARDCDPP0001Mapper.java
@@ -0,0 +1,8 @@
+package com.hyundaimotors.hmb.cdppapp.mapper;
+
+import com.hyundaimotors.hmb.cdppapp.dto.IFHMBZICARDCDPP0001Dto;
+
+public interface IFHMBZICARDCDPP0001Mapper {
+    
+    public IFHMBZICARDCDPP0001Dto getObject(IFHMBZICARDCDPP0001Dto dto)throws Exception;
+}
