3f7f60cf094e546aa185584a77cfc25e2aeb4458
Jinhyeok Choi <jhchoi@anchors-biz.com>
2023-07-27 03:48:17 +0900

    IFHMBDMSCDPP0004 add

---
+++ src/main/java/com/hyundaimotors/hmb/cdppapp/dto/IFHMBDMSCDPP0004Dto.java
@@ -0,0 +1,69 @@
+package com.hyundaimotors.hmb.cdppapp.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class IFHMBDMSCDPP0004Dto {
+    private String rowId;
+    private String date;
+    private String dealerCode;
+    private String apiKey;
+    private String statusLead;
+    private String errorspcCode;
+    private String errorspcMessage;
+    private String nome;
+    private String sobrenome;
+    private String genero;
+    private String dataNascimento;
+    private String telefone;
+    private String email;
+    private String cpf;
+    private String cep;
+    private String endereco;
+    private String idconnex;
+    private String dataPrevistaCarroNovo;
+    private String modelo;
+    private String motor;
+    private String transmissao;
+    private String versao;
+    private String cor;
+    private String preco;
+    private String protocolo;
+    private String codigoConcessionaria;
+    private String origem;
+    private String metodo;
+    private String dataAbertura;
+    private String nomeResponsavel;
+    private String status;
+    private String prioridade;
+    private String identificacaoPCD;
+    private String tipoContato;
+    private String clienteHyundai;
+    private String usadoNaTroca;
+    private String optInCotacao;
+    private String optInContato;
+    private String nomeConcessionaria;
+    private String StatusConcessionaria;
+    private String comentario;
+    private String metodoContato;
+    private String observacoesContato;
+    private String dadosCampanha;
+    private String querFinanciar;
+    private String tipoFinanciamento;
+    private String valorEntradaFinanciamento;
+    private String qtdParcelaFinanciamento;
+    private String temperatureNum;
+    private String temperature;
+    private String firstInteractionDate;
+    private String saleStatus;
+    private String reason;
+    private String result;
+    private String saleCPF;
+    private String endDate;
+    private String dlrReceiveDate;
+    private String dlrResponseTime;
+
+}
