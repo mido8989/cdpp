78d8df642064ab68de638e2d18925a45a8f91a30
Anchors Seohyun Han <shhan@anchors-biz.com>
2023-08-02 21:36:53 +0900

    IFHMBZICARDCDPP0007 adds

--- src/main/java/com/hyundaimotors/hmb/cdppapp/dto/IFHMBZICARDCDPP0007Dto.java
+++ src/main/java/com/hyundaimotors/hmb/cdppapp/dto/IFHMBZICARDCDPP0007Dto.java
@@ -6,14 +6,11 @@ import lombok.Setter;
 @Getter
 @Setter
 public class IFHMBZICARDCDPP0007Dto {
-
     private String assetId;
     private String contactId;
     private String relationShip;
     private String hkmeRetailDate;
     private String errorSpcCode;
     private String errorSpcMessage;
-    private String numAffRow;
-
-    
+    private String numAffRow;    
 }
