c4124d5057bde742dbd63c4b628e0e5ed056a4a2
Jinhyeok Choi <jhchoi@anchors-biz.com>
2023-08-03 16:32:42 +0900

    IFHMBINNOCEANCDPP0027 Adds

---
+++ src/main/java/com/hyundaimotors/hmb/cdppapp/dto/IFHMBINNOCEANCDPP0028Dto.java
@@ -0,0 +1,18 @@
+package com.hyundaimotors.hmb.cdppapp.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class IFHMBINNOCEANCDPP0028Dto {
+    private String contactId;
+    private String partnerName;
+    private String partnerCNPJ;
+    private String partnerExternalId;
+    private String benefitName;
+    private String benefitURL;
+    private String appSource;
+    private String errorSpcCode;
+    private String errorSpcMessage;
+}
