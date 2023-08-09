36836115fe84c292fdcdb33b0882b0166064e9be
Jinhyeok Choi <jhchoi@anchors-biz.com>
2023-08-09 23:07:27 +0900

    가비지 소스 정리

--- src/main/java/com/hyundaimotors/hmb/cdppapp/Repository/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.hyundaimotors.hmb.cdppapp.Repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.hyundaimotors.hmb.cdppapp.dto.TestDto;
-
-public interface UserRepo extends CrudRepository<TestDto, TestDto> {
-}
