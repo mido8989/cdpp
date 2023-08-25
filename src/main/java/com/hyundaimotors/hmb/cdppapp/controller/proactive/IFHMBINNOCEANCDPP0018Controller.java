97fd545321e10a9fc7f278591b09965032af232e
Jinhyeok Choi <jhchoi@anchors-biz.com>
2023-08-25 14:21:57 +0900

    IFHMBINNOCEANCDPP0019 Add IFHMBINNOCEANCDPP0018 Move File

---
+++ src/main/java/com/hyundaimotors/hmb/cdppapp/controller/proactive/IFHMBINNOCEANCDPP0018Controller.java
@@ -0,0 +1,57 @@
+package com.hyundaimotors.hmb.cdppapp.controller.proactive;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.ObjectUtils;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hyundaimotors.hmb.cdppapp.dto.IFHMBINNOCEANCDPP0018Dto;
+import com.hyundaimotors.hmb.cdppapp.dto.IFHMBINNOCEANCDPP0013.IFHMBINNOCEANCDPP0013Dto;
+import com.hyundaimotors.hmb.cdppapp.payload.IFHMBINNOCEANCDPP0001Payload;
+import com.hyundaimotors.hmb.cdppapp.payload.IFHMBINNOCEANCDPP0018Payload;
+import com.hyundaimotors.hmb.cdppapp.payload.IFHMBINNOCEANCDPP0032Payload;
+import com.hyundaimotors.hmb.cdppapp.payload.IFHMBINNOCEANCDPP0032Payload.SearchContactTD;
+import com.hyundaimotors.hmb.cdppapp.service.IFHMBINNOCEANCDPP0018Service;
+import com.hyundaimotors.hmb.cdppapp.service.IFHMBINNOCEANCDPP0021Service;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+
+@Tag(name = "HMB MNTSRVC Customer Schedule", description = "This web service can be used to query the customer's maintenance service schedule.")
+@RestController
+@RequiredArgsConstructor
+public class IFHMBINNOCEANCDPP0018Controller {
+    @Autowired
+    private final IFHMBINNOCEANCDPP0018Service service;
+
+    private final ModelMapper defaultMapper;
+
+    @Operation(summary = "HMB MNTSRVC Customer Schedule", description = "HMB MNTSRVC Customer Schedule ")
+    @ApiResponse(content = @Content(schema = @Schema(implementation = IFHMBINNOCEANCDPP0018Payload.Response.class)))
+    @PostMapping(value = "/api/v1/HMBMNTSRVCCustomerSchedule")
+    public Object getList(@RequestBody IFHMBINNOCEANCDPP0018Payload.Request request)throws Exception{
+        ModelMapper modelMapper = new ModelMapper();
+    
+        IFHMBINNOCEANCDPP0018Dto dto = defaultMapper.map(request, IFHMBINNOCEANCDPP0018Dto.class);
+
+        List<IFHMBINNOCEANCDPP0018Dto> resultList = new ArrayList<>();
+
+        resultList = service.getList(dto);
+        
+        return IFHMBINNOCEANCDPP0018Payload.Response.builder()
+                .errorSpcCode("0")
+                .errorSpcMessage("OK")
+                .listOfServices(resultList.size() > 0 ? defaultMapper.map(resultList, new TypeToken<List<IFHMBINNOCEANCDPP0018Payload.ListOfServices>>() {}.getType()) : null)
+                .build();
+    }    
+}
