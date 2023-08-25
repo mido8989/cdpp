602e3a8ea42da7de0553f8818b5d4f997722e3fd
Jinhyeok Choi <jhchoi@anchors-biz.com>
2023-08-25 14:22:16 +0900

    IFHMBINNOCEANCDPP0018 ADD IFHMBINNOCEANCDPP0019 File Move

---
+++ src/main/java/com/hyundaimotors/hmb/cdppapp/payload/IFHMBINNOCEANCDPP0019Payload.java
@@ -0,0 +1,40 @@
+package com.hyundaimotors.hmb.cdppapp.payload;
+
+import java.util.List;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+public class IFHMBINNOCEANCDPP0019Payload {
+    
+    @Schema(name = "IFHMBINNOCEANCDPP0019Payload.Request")
+    @Getter
+    @Setter
+    public static class Request{
+        private String dealerCode;
+        private String year;
+        private String month;
+        private String source;
+
+    }
+
+    @Schema(name = "IFHMBINNOCEANCDPP0019Payload.Request")
+    @Getter
+    @Setter
+    @SuperBuilder
+    public static class Response{
+        private String errorSpcCode;
+        private String errorSpcMessage;
+        List<ListOfDays> listOfDays;
+    }
+
+    @Schema(name = "ListOfDays")
+    @Getter
+    @Setter
+    public static class ListOfDays{
+        private String description;
+        private String available;
+    }
+}
