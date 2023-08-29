d24cb3eec806eeb5d6f8143c0eb1c014305654b2
Anchors Seohyun Han <shhan@anchors-biz.com>
2023-08-29 22:18:59 +0900

    IFHMBSAPEAICDPP0035 작업

---
+++ src/main/java/com/hyundaimotors/hmb/cdppapp/dto/IFHMBSAPEAICDPP0035Dto.java
@@ -0,0 +1,15 @@
+package com.hyundaimotors.hmb.cdppapp.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class IFHMBSAPEAICDPP0035Dto {
+    private String rowId;
+    private String url;
+    private String srNumber;
+    private String attachmentId;
+    private String errorSpcCode;
+    private String errorSpcMessage;
+}
---
+++ src/main/java/com/hyundaimotors/hmb/cdppapp/mapper/IFHMBSAPEAICDPP0035Mapper.java
@@ -0,0 +1,9 @@
+package com.hyundaimotors.hmb.cdppapp.mapper;
+
+import com.hyundaimotors.hmb.cdppapp.dto.IFHMBSAPEAICDPP0035Dto;
+
+public interface IFHMBSAPEAICDPP0035Mapper {
+
+    public void insertObject(IFHMBSAPEAICDPP0035Dto dto)throws Exception;
+    
+}
