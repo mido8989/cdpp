440efe7a37f0efbc2f3cdbdcbbff9ae21db9c2b4
Jinhyeok Choi <jhchoi@anchors-biz.com>
2023-09-03 20:01:42 +0900

    IFHMBSMARTERSCDPP0036 Adds Process And Replica

--- src/main/java/com/hyundaimotors/hmb/cdppapp/mapper/IFHMBSMARTERSCDPP0036Mapper.java
+++ src/main/java/com/hyundaimotors/hmb/cdppapp/mapper/IFHMBSMARTERSCDPP0036Mapper.java
@@ -1,5 +1,7 @@
 package com.hyundaimotors.hmb.cdppapp.mapper;
 
+import java.util.HashMap;
+
 import com.hyundaimotors.hmb.cdppapp.dto.IFHMBINNOCEANCDPP0034Dto;
 import com.hyundaimotors.hmb.cdppapp.dto.IFHMBSMARTERSCDPP0036Dto;
 
@@ -11,7 +13,7 @@ public interface IFHMBSMARTERSCDPP0036Mapper {
 
     public void insertMessage(IFHMBSMARTERSCDPP0036Dto dto)throws Exception;
 
-    /*public void transferProcess(HashMap<String, String> parMap)throws Exception;
+    public void transferProcess(HashMap<String, String> parMap)throws Exception;
 
-    public void transferReplica(HashMap<String, String> parMap)throws Exception;*/
+    public void transferReplica(HashMap<String, String> parMap)throws Exception;
 }
