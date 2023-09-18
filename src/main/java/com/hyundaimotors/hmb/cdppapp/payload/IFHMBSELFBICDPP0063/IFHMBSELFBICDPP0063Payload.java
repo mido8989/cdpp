0240676e44f76ee6b1e71b04dce4b10c7cb866dd
Jinhyeok Choi <jhchoi@anchors-biz.com>
2023-09-19 00:40:52 +0900

    IFHMBSELFBICDPP0063 Create -> legacy_created Change

--- src/main/java/com/hyundaimotors/hmb/cdppapp/payload/IFHMBSELFBICDPP0063/IFHMBSELFBICDPP0063Payload.java
+++ src/main/java/com/hyundaimotors/hmb/cdppapp/payload/IFHMBSELFBICDPP0063/IFHMBSELFBICDPP0063Payload.java
@@ -3,6 +3,7 @@ package com.hyundaimotors.hmb.cdppapp.payload.IFHMBSELFBICDPP0063;
 import java.util.List;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,14 +14,19 @@ public class IFHMBSELFBICDPP0063Payload {
     @Getter
     @Setter
     public static class Request{
-        @NotNull
         @Schema(description = "MM/DD/YYYY, billing date to capture.", example = "2018-10-30")
+        @NotNull
+        @NotEmpty
         private String dateInput;
-        @NotNull
+
         @Schema(description = "Y or N. If you select Y, then Siebel will get all VIN searching them by Created Field. If you select N, then Siebel will get all VIN searching them by Sales date field.", example = "Y")
-        private String createdOption;
         @NotNull
+        @NotEmpty
+        private String createdOption;
+
         @Schema(description = "1=All Sales, 2=Different from Direct Sales, 3=Direct Sales", example = "1")
+        @NotNull
+        @NotEmpty
         private String seq;
         @Schema(description = "Vehicle number", example = "9BHGA811BKP099943")
         private String vin;
