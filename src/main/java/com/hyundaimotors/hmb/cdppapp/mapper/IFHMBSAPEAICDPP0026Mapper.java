1b5a97d81fe0fde81b76ef86639e7fabfdbb10b7
Anchors Seohyun Han <shhan@anchors-biz.com>
2023-09-21 16:15:05 +0900

    IF_HMB_SAPEAI_CDPP_0026_MOD 작업

--- src/main/java/com/hyundaimotors/hmb/cdppapp/mapper/IFHMBSAPEAICDPP0026Mapper.java
+++ src/main/java/com/hyundaimotors/hmb/cdppapp/mapper/IFHMBSAPEAICDPP0026Mapper.java
@@ -3,18 +3,16 @@ package com.hyundaimotors.hmb.cdppapp.mapper;
 import java.util.HashMap;
 import java.util.List;
 
-import com.hyundaimotors.hmb.cdppapp.dto.IFHMBSAPEAICDPP0026Dto;
+import com.hyundaimotors.hmb.cdppapp.dto.IFHMBSAPEAICDPP0026.ListOfProductDto;
 
 public interface IFHMBSAPEAICDPP0026Mapper {
     
-    public int insertList(List<IFHMBSAPEAICDPP0026Dto> list)throws Exception;
+    public void insertProductList(ListOfProductDto dto)throws Exception;
+    
+    public void transferProcess(HashMap<String, String[]> parMap)throws Exception;
 
-    public int updateList(List<IFHMBSAPEAICDPP0026Dto> list)throws Exception;
+    public List<String> getProcIdList(HashMap<String, String[]> parMap)throws Exception;
 
-    public void transferProcess(HashMap<String, String> parMap)throws Exception;
-
-    public String processPoductId(String externalId)throws Exception;
-
-    public void transferReplica(HashMap<String, String> parMap)throws Exception;
+    public void transferReplica(HashMap<String, String[]> parMap)throws Exception;
 
 }
