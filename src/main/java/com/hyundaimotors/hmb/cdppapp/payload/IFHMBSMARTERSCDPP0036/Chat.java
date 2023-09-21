9bd204fc1b25d0b6598a224e6ce5f95dc11ce6b5
Jinhyeok Choi <jhchoi@anchors-biz.com>
2023-09-21 13:15:39 +0900

    NotNull Check Update

--- src/main/java/com/hyundaimotors/hmb/cdppapp/payload/IFHMBSMARTERSCDPP0036/Chat.java
+++ src/main/java/com/hyundaimotors/hmb/cdppapp/payload/IFHMBSMARTERSCDPP0036/Chat.java
@@ -1,6 +1,8 @@
 package com.hyundaimotors.hmb.cdppapp.payload.IFHMBSMARTERSCDPP0036;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,6 +11,8 @@ import lombok.Setter;
 @Setter
 public class Chat {
     @Schema(description = "Chat Code", example = "CH0006")
+    @NotNull
+    @NotEmpty
     private String chatCode;
     @Schema(description = "Date Chat", example = "2020-01-31  10:31:02 AM")
     private String date;
