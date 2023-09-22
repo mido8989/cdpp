fc7807e8ecca55a94b83975959a1d630883b0545
Jinhyeok Choi <jhchoi@anchors-biz.com>
2023-09-22 19:10:06 +0900

    IFHMBINNOCEANCDPP0001 Update

--- src/main/java/com/hyundaimotors/hmb/cdppapp/payload/IFHMBINNOCEANCDPP0001Payload.java
+++ src/main/java/com/hyundaimotors/hmb/cdppapp/payload/IFHMBINNOCEANCDPP0001Payload.java
@@ -33,24 +33,34 @@ public class IFHMBINNOCEANCDPP0001Payload {
     @Setter
     public static class Response{
         @Schema(description = "cpfNumber", example = "12337343006")
+        @JsonProperty("CPF")
         private String cpf;
         @Schema(description = "ContactId", example = "1-90ABC")
+        @JsonProperty("ContactId")
         private String contactId;
         @Schema(description = "Full name", example = "Jo\u00E3o de Jesus Atan\u00E1zio")
+        @JsonProperty("Name")
         private String name;
         @Schema(description = "personCell", example = "5511976588820")
+        @JsonProperty("PersonCell")
         private String personCell;
         @Schema(description = "protocolNumber", example = "HMB98949022323")
+        @JsonProperty("ProtocolNumber")
         private String protocolNumber;
         @Schema(description = "protocolOwnerLogin", example = "HMBJBSOUZA")
+        @JsonProperty("ProtocolOwnerLogin")
         private String protocolOwnerLogin;
         @Schema(description = "protocolOwnerName", example = "Joaquim de Sousa")
+        @JsonProperty("ProtocolOwnerName")
         private String protocolOwnerName;
         @Schema(description = "protocolStatus", example = "In Progress BO")
+        @JsonProperty("ProtocolStatus")
         private String protocolStatus;
         @Schema(description = "errorSpcCode", example = "SBL-12Z3Z")
+        @JsonProperty("Error_spcCode")
         private String errorSpcCode;
         @Schema(description = "errorSpcMessage", example = "Erro ao executar a tarefa  X do Connex.")
+        @JsonProperty("Error_spcMessage")
         private String errorSpcMessage;
     }
 }
