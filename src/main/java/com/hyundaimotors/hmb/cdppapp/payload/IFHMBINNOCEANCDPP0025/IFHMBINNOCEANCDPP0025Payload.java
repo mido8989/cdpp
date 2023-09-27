25d0513838389c12ad5d51a142acdebafc8c1438
Jinhyeok Choi <jhchoi@anchors-biz.com>
2023-09-27 15:52:30 +0900

    IFHMBINNOCEANCDPP0025 Atrribute Update

--- src/main/java/com/hyundaimotors/hmb/cdppapp/payload/IFHMBINNOCEANCDPP0025/IFHMBINNOCEANCDPP0025Payload.java
+++ src/main/java/com/hyundaimotors/hmb/cdppapp/payload/IFHMBINNOCEANCDPP0025/IFHMBINNOCEANCDPP0025Payload.java
@@ -1,5 +1,7 @@
 package com.hyundaimotors.hmb.cdppapp.payload.IFHMBINNOCEANCDPP0025;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
@@ -18,8 +20,10 @@ public class IFHMBINNOCEANCDPP0025Payload {
     @Setter
     public static class Response{
         @Schema(description = "Returns 0 when success.", example = "0")
+        @JsonProperty("Error_spcCode")
         private String ErrorSpcCode;
         @Schema(description = "Returns OK when success.", example = "OK")
+        @JsonProperty("Error_spcMessage")
         private String ErrorSpcMessage;
         @Schema(description = "Protocol number.", example = "HMB8761468242")
         private String srNumber;
