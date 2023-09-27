2d4c0244efd7412a7df644b4f21363c364132b0b
Anchors Seohyun Han <shhan@anchors-biz.com>
2023-09-27 14:28:17 +0900

    IFHMBINNOCEANCDPP0028_MOD 작업

--- src/main/java/com/hyundaimotors/hmb/cdppapp/mapper/IFHMBINNOCEANCDPP0028Mapper.java
+++ src/main/java/com/hyundaimotors/hmb/cdppapp/mapper/IFHMBINNOCEANCDPP0028Mapper.java
@@ -6,11 +6,11 @@ import com.hyundaimotors.hmb.cdppapp.dto.IFHMBINNOCEANCDPP0028Dto;
 
 public interface IFHMBINNOCEANCDPP0028Mapper {
     
-    public int checkRowId(IFHMBINNOCEANCDPP0028Dto dto)throws Exception;
+    public String getProcRowId(IFHMBINNOCEANCDPP0028Dto dto)throws Exception;
 
     public void insertObject(IFHMBINNOCEANCDPP0028Dto dto)throws Exception;
 
-    public void updateObject(IFHMBINNOCEANCDPP0028Dto dto)throws Exception;
-
     public void transferProcess(HashMap<String, String> parMap)throws Exception;
+
+    public void transferReplica(HashMap<String, String> parMap)throws Exception;
 }
