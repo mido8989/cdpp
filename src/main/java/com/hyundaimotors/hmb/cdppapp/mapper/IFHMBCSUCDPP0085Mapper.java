0077a0fa6254a79972f66c5ad9ed300a21853a30
Jinhyeok Choi <jhchoi@anchors-biz.com>
2023-09-28 19:23:05 +0900

    IFHMBCSUCDPP0085 Already Add

---
+++ src/main/java/com/hyundaimotors/hmb/cdppapp/mapper/IFHMBCSUCDPP0085Mapper.java
@@ -0,0 +1,11 @@
+package com.hyundaimotors.hmb.cdppapp.mapper;
+
+import java.util.List;
+
+import com.hyundaimotors.hmb.cdppapp.dto.IFHMBCSUCDPP0085.ChannelPartnerCSUDto;
+import com.hyundaimotors.hmb.cdppapp.dto.IFHMBCSUCDPP0085.IFHMBCSUCDPP0085Dto;
+
+public interface IFHMBCSUCDPP0085Mapper {
+    
+    public List<ChannelPartnerCSUDto> getChannelObject(IFHMBCSUCDPP0085Dto dto)throws Exception;
+}
