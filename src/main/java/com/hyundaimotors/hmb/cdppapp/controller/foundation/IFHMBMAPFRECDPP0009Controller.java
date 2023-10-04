d3d19d120aafa6542f77227ffda32c493b1a58f5
Anchors Seohyun Han <shhan@anchors-biz.com>
2023-10-04 13:20:56 +0900

    IFHMBMAPFRECDPP0009 Dummy 방식으로 변경

--- src/main/java/com/hyundaimotors/hmb/cdppapp/controller/foundation/IFHMBMAPFRECDPP0009Controller.java
+++ src/main/java/com/hyundaimotors/hmb/cdppapp/controller/foundation/IFHMBMAPFRECDPP0009Controller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.hyundaimotors.hmb.cdppapp.dto.IFHMBMAPFRECDPP0009Dto;
+import com.hyundaimotors.hmb.cdppapp.dto.IFHMBMAPFRECDPP0009.IFHMBMAPFRECDPP0009Dto;
 import com.hyundaimotors.hmb.cdppapp.payload.IFHMBMAPFRECDPP0009.IFHMBMAPFRECDPP0009Payload;
 import com.hyundaimotors.hmb.cdppapp.service.IFHMBMAPFRECDPP0009Service;
 
--- src/main/java/com/hyundaimotors/hmb/cdppapp/mapper/IFHMBMAPFRECDPP0009Mapper.java
+++ src/main/java/com/hyundaimotors/hmb/cdppapp/mapper/IFHMBMAPFRECDPP0009Mapper.java
@@ -2,26 +2,19 @@ package com.hyundaimotors.hmb.cdppapp.mapper;
 
 import java.util.HashMap;
 
-import com.hyundaimotors.hmb.cdppapp.dto.IFHMBMAPFRECDPP0009Dto;
-import com.hyundaimotors.hmb.cdppapp.dto.IFHMBMAPFRECDPP0009RSAServicePayDto;
+import com.hyundaimotors.hmb.cdppapp.dto.IFHMBMAPFRECDPP0009.IFHMBMAPFRECDPP0009Dto;
 
 public interface IFHMBMAPFRECDPP0009Mapper {
     
-    public int getAssetCount(IFHMBMAPFRECDPP0009Dto dto)throws Exception;
+    public String getRSAId(IFHMBMAPFRECDPP0009Dto dto)throws Exception;
 
-    public int getCheckoutRowId(IFHMBMAPFRECDPP0009Dto dto)throws Exception;
+    public void deleteList(IFHMBMAPFRECDPP0009Dto dto)throws Exception;
 
-    public int insertObject(IFHMBMAPFRECDPP0009Dto dto)throws Exception;
+    public void insertObject(IFHMBMAPFRECDPP0009Dto dto)throws Exception;
 
-    public int updateParRowId(IFHMBMAPFRECDPP0009Dto dto)throws Exception;
-
-    public int updateObject(IFHMBMAPFRECDPP0009Dto dto)throws Exception;
-
-    public int rasServiceCheck(IFHMBMAPFRECDPP0009RSAServicePayDto dto)throws Exception;
-
-    public int insertResSetvice(IFHMBMAPFRECDPP0009RSAServicePayDto dto)throws Exception;
-
-    public int updateResSetvice(IFHMBMAPFRECDPP0009RSAServicePayDto dto)throws Exception;
+    public void insertResSetvice(IFHMBMAPFRECDPP0009Dto dto)throws Exception;
 
     public void transferProcess(HashMap<String, String> parMap)throws Exception;
+
+    public void transferReplica(HashMap<String, String> parMap)throws Exception;
 }
