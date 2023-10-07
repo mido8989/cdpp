1462dec9e957a1d07350e7aa85b7d8ed258ee9f3
Jinhyeok Choi <jhchoi@anchors-biz.com>
2023-10-08 08:30:26 +0900

    IFHMBINNOCEANCDPP0088 Adds

---
+++ src/main/java/com/hyundaimotors/hmb/cdppapp/payload/IFHMBINNOCEANCDPP0088/AmaroCSISurveyAnswer.java
@@ -0,0 +1,22 @@
+package com.hyundaimotors.hmb.cdppapp.payload.IFHMBINNOCEANCDPP0088;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AmaroCSISurveyAnswer {
+    @Schema(description = "Question unique identifier code.", example = "1365")
+    private String questionId;
+    @Schema(description = "Question numeric sequence in the survey.", example = "10")
+    private String questionCode;
+    @Schema(description = "Question description.", example = "Consta em nosso banco de dados que o(a) sr(a) adquiriu o veículo ##VEICULO##, na concessionária  ##FILIAL##. O(A) sr.(a) foi responsável e esteve presente pessoalmente durante as negociações de compra assim como no momento da retirada do veículo?")
+    private String questionComments;
+    @Schema(description = "Answer description.", example = "<Long>")
+    private String answerDescription;
+    @Schema(description = "Answer numeric weight value.", example = "100")
+    private String answerValue;
+    @Schema(description = "Answer complementary information.", example = "Outros (Verbalizar)")
+    private String answerComplement;
+}
