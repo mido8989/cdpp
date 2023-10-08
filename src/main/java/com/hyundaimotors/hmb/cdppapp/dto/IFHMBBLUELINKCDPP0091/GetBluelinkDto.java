0ae4653fa7ad80b107335ae6a171d6593f5a1a75
Jinhyeok Choi <jhchoi@anchors-biz.com>
2023-10-09 06:27:41 +0900

    IFHMBBLUELINKCDPP0091 Adds

---
+++ src/main/java/com/hyundaimotors/hmb/cdppapp/dto/IFHMBBLUELINKCDPP0091/GetBluelinkDto.java
@@ -0,0 +1,14 @@
+package com.hyundaimotors.hmb.cdppapp.dto.IFHMBBLUELINKCDPP0091;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GetBluelinkDto {
+    private String assetNum;
+    private String assetDate;
+    private String type;
+    private String firstActivation;
+    private String eventDate;
+}
---
+++ src/main/java/com/hyundaimotors/hmb/cdppapp/service/IFHMBBLUELINKCDPP0091Service.java
@@ -0,0 +1,7 @@
+package com.hyundaimotors.hmb.cdppapp.service;
+
+import com.hyundaimotors.hmb.cdppapp.dto.IFHMBBLUELINKCDPP0091.IFHMBBLUELINKCDPP0091Dto;
+
+public interface IFHMBBLUELINKCDPP0091Service {
+    public IFHMBBLUELINKCDPP0091Dto insertObject(IFHMBBLUELINKCDPP0091Dto dto)throws Exception;
+}
