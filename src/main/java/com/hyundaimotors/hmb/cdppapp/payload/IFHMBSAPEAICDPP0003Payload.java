d54d68db394ee4ceb76acc89ec71ac5de0638800
Anchors Seohyun Han <shhan@anchors-biz.com>
2023-10-16 16:53:54 +0900

    IFHMBSAPEAICDPP0003 Attribute 수정

--- src/main/java/com/hyundaimotors/hmb/cdppapp/payload/IFHMBSAPEAICDPP0003Payload.java
+++ src/main/java/com/hyundaimotors/hmb/cdppapp/payload/IFHMBSAPEAICDPP0003Payload.java
@@ -1,5 +1,7 @@
 package com.hyundaimotors.hmb.cdppapp.payload;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
@@ -13,24 +15,32 @@ public class IFHMBSAPEAICDPP0003Payload {
     public static class Request{
         @Schema(description = "FSC OCN unique code. This is the key field for integration.", example = "1-RGHT2")
         @NotNull
+        @JsonProperty("Name")
         private String name;
         @Schema(description = "Name of external system.", example = "")
+        @JsonProperty("Source")
         private String source;
         @Schema(description = "Connex product type. It must be a literal value for the purpose of this project: Vehicle.", example = "Vehicle")
+        @JsonProperty("Type")
         private String type;
         @Schema(description = "Vehicle description: E.g.:", example = "HB20 1.6L COMFORT PLUS MT")
+        @JsonProperty("Part")
         private String part;
         @Schema(description = "Vehicle advanced description. E.g.:", example = "Ar-condicionado")
         private String advdesc; 
         @Schema(description = "Vehicle model year.", example = "2010")
+        @JsonProperty("ModelYear")
         private int modelYear;
         @Schema(description = "Vehicle chassi model.", example = "Hatchback")
         private String chassiModel; 
         @Schema(description = "Vehicle body style.", example = " 998cc")
+        @JsonProperty("BodyStyle")
         private String bodyStyle;
         @Schema(description = "Vehicle engine capacity description.", example = " DOHC")
+        @JsonProperty("HMBCylindercapacity")
         private String hmbCylindercapacity;
         @Schema(description = "Vehicle engine type description. ", example = "MPI")
+        @JsonProperty("Engine")
         private String engine;
         @Schema(description = "Vehicle engine friendly description.", example = "1.0 Turbo")
         private String engineDesc;
@@ -39,13 +49,21 @@ public class IFHMBSAPEAICDPP0003Payload {
         @Schema(description = "Vehicle transmission friendly description. ", example = "Manual 6 marchas")
         private String transmissionDesc;
         @Schema(description = "Vehicle transmission. ", example = "A.M")
+        @JsonProperty("Transmission")
         private String transmission;
         @Schema(description = "Vehicle fuel type description.", example = "Flex")
+        @JsonProperty("Fuel")
         private String fuel;
         @Schema(description = "Vehicle door amount. ", example = "5")
         private int doorsAmnt;
         @Schema(description = "Vehicle suggested price", example = "50000")
         private String suggestedPrice;
+        @Schema(description = "Description", example = "Rede de porta-malas")
+        @JsonProperty("Description")
+        private String description;
+        @Schema(description = "ProductTypeCode", example = "Product")
+        @JsonProperty("ProductTypeCode")
+        private String productTypeCode;
     }    
 
     @Schema(name = "IFHMBSAPEAICDPP0003Payload.Response")
@@ -53,8 +71,10 @@ public class IFHMBSAPEAICDPP0003Payload {
     @Setter
     public static class Response{
         @Schema(description = "Connex error code. Code 200 means success.", example = "200")
+        @JsonProperty("Error_spcCode")
         private String errorSpcCode;
         @Schema(description = "Connex error message.", example = "success")
+        @JsonProperty("Error_spcMessage")
         private String errorSpcMessage;
     }
 }
