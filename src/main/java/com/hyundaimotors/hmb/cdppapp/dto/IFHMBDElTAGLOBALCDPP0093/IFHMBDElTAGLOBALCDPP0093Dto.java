59fde70152081cb97d0ade07f224e79a816bb8a6
Jinhyeok Choi <jhchoi@anchors-biz.com>
2023-10-19 00:58:18 +0900

    IFHMBDElTAGLOBALCDPP0093 Adds

---
+++ src/main/java/com/hyundaimotors/hmb/cdppapp/dto/IFHMBDElTAGLOBALCDPP0093/IFHMBDElTAGLOBALCDPP0093Dto.java
@@ -0,0 +1,14 @@
+package com.hyundaimotors.hmb.cdppapp.dto.IFHMBDElTAGLOBALCDPP0093;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class IFHMBDElTAGLOBALCDPP0093Dto {
+    List<RentalResultDto> listOfRentalResult;
+    private String errorSpcCode;
+    private String errorSpcMessage;
+}
