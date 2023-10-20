f7468fff4f6ce467cd986a73f49438180f1b189f
Jinhyeok Choi <jhchoi@anchors-biz.com>
2023-10-21 00:03:47 +0900

    InboundRepair Attribute Change

--- src/main/java/com/hyundaimotors/hmb/cdppapp/payload/IFHMBSAPCDPP0079/InboundRepairParts.java
+++ src/main/java/com/hyundaimotors/hmb/cdppapp/payload/IFHMBSAPCDPP0079/InboundRepairParts.java
@@ -10,28 +10,39 @@ import lombok.Setter;
 @Setter
 public class InboundRepairParts {
     @Schema(description = "soid", example = "0002345")
+    @JsonProperty("CODIGO_OS")
     private String soid;
     @Schema(description = "dealercode", example = "B05AM12345")
+    @JsonProperty("DEALER_CODE")
     private String dealercode;
     @Schema(description = "vincode", example = "9BH12345678901234")
+    @JsonProperty("CHASSI")
     private String vincode;
     @Schema(description = "itmnum", example = "0001")
+    @JsonProperty("COD_ITEM")
     private String itmnum;
     @Schema(description = "sotype", example = "CSR-GSS")
+    @JsonProperty("TIPO_OS")
     private String sotype;
     @Schema(description = "pncode", example = "4300780900")
+    @JsonProperty("COD_PRODUTO")
     private String pncode;
     @Schema(description = "zdesc", example = "SHELL HELIX ULTRA AH 5W-30 SN")
+    @JsonProperty("DESCRICAO_ITEM")
     private String zdesc;
     @Schema(description = "zqty", example = "2,900")
+    @JsonProperty("QUANTIDADE_ITEM")
     private float zqty;
     @Schema(description = "unit", example = "LT")
+    @JsonProperty("UNIDADE")
     private String unit;
     @Schema(description = "value", example = "71,70")
+    @JsonProperty("VALOR_UNITARIO_ITEM")
     private float value;
     @Schema(description = "total_Value", example = "207,93")
-    @JsonProperty("total_Value")
+    @JsonProperty("VALOR_TOTAL_ITEM")
     private float totalValue;
+    /*
     @Schema(description = "ernam", example = "AXAPIRFC01")
     private String ernam;
     @Schema(description = "erdat", example = "27092023")
@@ -44,5 +55,6 @@ public class InboundRepairParts {
     private String aedat;
     @Schema(description = "aezet", example = "183753")
     private String aezet;
+    */
 
 }
--- src/main/java/com/hyundaimotors/hmb/cdppapp/payload/IFHMBSAPCDPP0079/InboundRepairService.java
+++ src/main/java/com/hyundaimotors/hmb/cdppapp/payload/IFHMBSAPCDPP0079/InboundRepairService.java
@@ -1,5 +1,7 @@
 package com.hyundaimotors.hmb.cdppapp.payload.IFHMBSAPCDPP0079;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,27 +10,39 @@ import lombok.Setter;
 @Setter
 public class InboundRepairService {
     @Schema(description = "soid", example = "0002345")
+    @JsonProperty("CODIGO_OS")
     private String soid;
     @Schema(description = "dealercode", example = "B05AM12345")
+    @JsonProperty("DEALER_CODE")
     private String dealercode;
     @Schema(description = "vincode", example = "9BH12345678901234")
+    @JsonProperty("CHASSI")
     private String vincode;
     @Schema(description = "itmnum", example = "001")
+    @JsonProperty("COD_ITEM")
     private String itmnum;
     @Schema(description = "sotype", example = "CSP-ISP")
+    @JsonProperty("TIPO_OS")
     private String sotype;
     @Schema(description = "codsrc", example = "A02E5")
+    @JsonProperty("COD_SERVICO")
     private String codsrc;
     @Schema(description = "zdesc", example = "TEST")
+    @JsonProperty("DESCRICAO_ITEM")
     private String zdesc;
     @Schema(description = "zqty", example = "3")
+    @JsonProperty("QUANTIDADE_ITEM")
     private float zqty;
     @Schema(description = "unit", example = "H")
+    @JsonProperty("UNIDADE")
     private String unit;
     @Schema(description = "value", example = "120")
+    @JsonProperty("VALOR_UNITARIO_ITEM")
     private float value;
     @Schema(description = "totalValue", example = "360")
+    @JsonProperty("VALOR_TOTAL_ITEM")
     private float totalValue;
+    /* 
     @Schema(description = "ernam", example = "SAPUSR01")
     private String ernam;
     @Schema(description = "erdat", example = "27092023")
@@ -41,4 +55,5 @@ public class InboundRepairService {
     private String aedat;
     @Schema(description = "aezet", example = "183832")
     private String aezet;
+    */
 }
