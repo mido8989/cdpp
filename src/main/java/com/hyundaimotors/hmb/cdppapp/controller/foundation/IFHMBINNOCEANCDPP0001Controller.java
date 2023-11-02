295d7b488a0bc09a8e9fe1d5d59279bc1454f02a
jjeong100 <jjeoong100@naver.com>
2023-11-02 17:11:12 +0900

    Api Log Dev Test 필요

--- src/main/java/com/hyundaimotors/hmb/cdppapp/controller/foundation/IFHMBINNOCEANCDPP0001Controller.java
+++ src/main/java/com/hyundaimotors/hmb/cdppapp/controller/foundation/IFHMBINNOCEANCDPP0001Controller.java
@@ -1,5 +1,7 @@
 package com.hyundaimotors.hmb.cdppapp.controller.foundation;
 
+import java.util.UUID;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,7 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.hyundaimotors.hmb.cdppapp.dto.IFHMBINNOCEANCDPP0001Dto;
 import com.hyundaimotors.hmb.cdppapp.payload.IFHMBINNOCEANCDPP0001Payload;
+import com.hyundaimotors.hmb.cdppapp.service.ApiLogService;
 import com.hyundaimotors.hmb.cdppapp.service.IFHMBINNOCEANCDPP0001Service;
+import com.hyundaimotors.hmb.cdppapp.util.ApiLog;
+import com.hyundaimotors.hmb.cdppapp.util.ApiLogStep;
+import com.hyundaimotors.hmb.cdppapp.util.JsonUtils;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -21,28 +27,40 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 public class IFHMBINNOCEANCDPP0001Controller {
-
     private static final String IF_ID = "IF001";
+    
+    private final ApiLogService logService;
     
     @Autowired
     private final IFHMBINNOCEANCDPP0001Service service;
 
     private final ModelMapper defaultMapper;
 
-
-
     @Operation(summary = "HMB Get Contact WF", description = "HMB Get Contact WF")
     @ApiResponse(content = @Content(schema = @Schema(implementation = IFHMBINNOCEANCDPP0001Payload.Response.class)))
     @PostMapping(value = "/api/v1/HMBGetContactWF")
-    public Object getObject(@RequestBody IFHMBINNOCEANCDPP0001Payload.Request request)throws Exception{
-        ModelMapper modelMapper = new ModelMapper();
+    public Object getObject(@RequestBody IFHMBINNOCEANCDPP0001Payload.Request request) throws Exception {
+        UUID IF_TR_ID = UUID.randomUUID();
         
-        IFHMBINNOCEANCDPP0001Dto resultDto = new IFHMBINNOCEANCDPP0001Dto();
-
-        IFHMBINNOCEANCDPP0001Dto dto = defaultMapper.map(request, IFHMBINNOCEANCDPP0001Dto.class);
-
-        resultDto = service.getObject(dto);
-
-        return modelMapper.map(resultDto, IFHMBINNOCEANCDPP0001Payload.Response.class);
+        IFHMBINNOCEANCDPP0001Payload.Response response = new IFHMBINNOCEANCDPP0001Payload.Response();
+        ApiLog.logApi(logService, IF_ID, ApiLogStep.START, IF_TR_ID, JsonUtils.toJson(request));
+        ModelMapper modelMapper = new ModelMapper();
+        try {
+            IFHMBINNOCEANCDPP0001Dto resultDto = new IFHMBINNOCEANCDPP0001Dto();
+            IFHMBINNOCEANCDPP0001Dto dto = defaultMapper.map(request, IFHMBINNOCEANCDPP0001Dto.class);
+            
+            ApiLog.logApi(logService, IF_ID,ApiLogStep.STEP1, IF_TR_ID, null);
+            resultDto = service.getObject(dto);
+            ApiLog.logApi(logService, IF_ID,ApiLogStep.STEP2, IF_TR_ID, null);
+    
+            response = modelMapper.map(resultDto, IFHMBINNOCEANCDPP0001Payload.Response.class);
+            ApiLog.logApi(logService, IF_ID,ApiLogStep.FINISH, IF_TR_ID, JsonUtils.toJson(response));
+        }catch(Exception e) {
+        	response.setErrorSpcCode("500");
+            response.setErrorSpcMessage(e.getLocalizedMessage());
+            ApiLog.logApi(logService, IF_ID,ApiLogStep.FINISH, IF_TR_ID, JsonUtils.toJson(response), e);
+        }
+        
+        return response;
     }
 }
