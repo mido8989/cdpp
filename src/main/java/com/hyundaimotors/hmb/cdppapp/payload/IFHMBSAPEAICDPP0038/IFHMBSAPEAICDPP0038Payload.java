d719e666e33386c1a6afaef1d93ab4a9b2aa421d
Jinhyeok Choi <jhchoi@anchors-biz.com>
2023-11-02 13:35:55 +0900

    IFHMBSAPEAICDPP0038Payload ListOf Change

--- src/main/java/com/hyundaimotors/hmb/cdppapp/payload/IFHMBSAPEAICDPP0038/IFHMBSAPEAICDPP0038Payload.java
+++ src/main/java/com/hyundaimotors/hmb/cdppapp/payload/IFHMBSAPEAICDPP0038/IFHMBSAPEAICDPP0038Payload.java
@@ -56,7 +56,9 @@ public class IFHMBSAPEAICDPP0038Payload {
         private String visitdate;
         @Schema(description = "Visit Status", example = "Canceled")
         private String visitstat;
+        @JsonProperty("ListOfTemperature")
         List<Temperature> listOfTemperature;
+        @JsonProperty("ListOfInteraction")
         List<Interaction> listOfInteraction;
     }
 
