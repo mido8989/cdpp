49550730bfa4b8d940498280401db0ade415968d
Anchors Seohyun Han <shhan@anchors-biz.com>
2023-11-06 14:01:03 +0900

    IFHMBINNOCEANCDPP0023 valid 제거

--- src/main/java/com/hyundaimotors/hmb/cdppapp/controller/proactive/IFHMBINNOCEANCDPP0023Controller.java
+++ src/main/java/com/hyundaimotors/hmb/cdppapp/controller/proactive/IFHMBINNOCEANCDPP0023Controller.java
@@ -39,7 +39,7 @@ public class IFHMBINNOCEANCDPP0023Controller {
     @Operation(summary = "HMBMNTSRVCManageEmployeeAvailability", description = "HMB MNTSRVC Manage Employee Availability")
     @ApiResponse(content = @Content(schema = @Schema(implementation = IFHMBINNOCEANCDPP0023Payload.Response.class)))
     @PostMapping(value = "/api/v1/HMBMNTSRVCManageEmployeeAvailability")
-    public Object upsertObject(@Valid @RequestBody IFHMBINNOCEANCDPP0023Payload.Request request) throws Exception {
+    public Object upsertObject(@RequestBody IFHMBINNOCEANCDPP0023Payload.Request request) throws Exception {
 
         UUID IF_TR_ID = UUID.randomUUID();
         
