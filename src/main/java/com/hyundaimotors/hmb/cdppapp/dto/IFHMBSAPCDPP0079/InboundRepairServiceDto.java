f00958834585cc0d7b50e01861f5d50d6516e4c2
Jinhyeok Choi <jhchoi@anchors-biz.com>
2023-11-07 17:04:23 +0900

    Repair Order Field Update

--- src/main/java/com/hyundaimotors/hmb/cdppapp/dto/IFHMBSAPCDPP0079/InboundRepairServiceDto.java
+++ src/main/java/com/hyundaimotors/hmb/cdppapp/dto/IFHMBSAPCDPP0079/InboundRepairServiceDto.java
@@ -1,5 +1,8 @@
 package com.hyundaimotors.hmb.cdppapp.dto.IFHMBSAPCDPP0079;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,18 +12,13 @@ public class InboundRepairServiceDto {
     private String soid;
     private String dealercode;
     private String vincode;
-    private String itmnum;
     private String sotype;
     private String codsrc;
     private String zdesc;
     private float zqty;
-    private String unit;
     private float value;
-    private float totalValue;
-    private String ernam;
     private String erdat;
     private String erzet;
-    private String aenam;
     private String aedat;
     private String aezet;
 }
