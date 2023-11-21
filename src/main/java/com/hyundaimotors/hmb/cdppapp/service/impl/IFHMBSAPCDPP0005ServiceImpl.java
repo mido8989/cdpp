006c36d30bcdc6ceac77939e8cf5899683a8e0ae
jjeong100 <jjeoong100@naver.com>
2023-11-21 16:18:33 +0900

    /** * 20231121 * 조건 : (cnpjNumber) 있거나 (name,mainPhoneNumber,mainEmailAddress) 값이 있으면 upsert * 그밖에 에러 처리 메시지 에러 코드는 1 */

--- src/main/java/com/hyundaimotors/hmb/cdppapp/service/impl/IFHMBSAPCDPP0005ServiceImpl.java
+++ src/main/java/com/hyundaimotors/hmb/cdppapp/service/impl/IFHMBSAPCDPP0005ServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.hyundaimotors.hmb.cdppapp.dto.IFHMBSAPCDPP0005Dto;
 import com.hyundaimotors.hmb.cdppapp.mapper.IFHMBSAPCDPP0005Mapper;
 import com.hyundaimotors.hmb.cdppapp.service.IFHMBSAPCDPP0005Service;
+import com.hyundaimotors.hmb.cdppapp.util.JsonUtils;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,52 +22,46 @@ public class IFHMBSAPCDPP0005ServiceImpl implements IFHMBSAPCDPP0005Service{
     public IFHMBSAPCDPP0005Dto insertObject(IFHMBSAPCDPP0005Dto dto)throws Exception{
         IFHMBSAPCDPP0005Dto resulDto = new IFHMBSAPCDPP0005Dto();
 
-        mapper.insertObject(dto);
+        mapper.insertObject(dto); //=> return row_id
         
         /**
-         * 20231110
-         * 조건 : cnpjNumber 값이 있고 foundAccountIdbyCnpj조회 값이 있으면 update
-         *       또는 name,mainPhoneNumber,mainEmailAddress 값이 있고 foundAccountIdbyCnpj조회 값이 있으면 update
-         * 나머지 insert
+         * 20231121
+         * 조건 : (cnpjNumber) 있거나 (name,mainPhoneNumber,mainEmailAddress) 값이 있으면 upsert
+         * 그밖에 에러 처리 메시지 에러 코드는 1
          */
-        String foundAccountIdbyCnpj = null;
+        boolean isValidation = false;
+        
         if(!isNull(dto.getCnpjNumber())) { //update
-            foundAccountIdbyCnpj = mapper.foundAccountIdbyCnpj(dto);
+            isValidation = true;
         }else {
-            if(!isNull(dto.getName()) && !isNull(dto.getMainPhoneNumber()) && !isNull(dto.getMainEmailAddress())) {
-                foundAccountIdbyCnpj = mapper.foundAccountIdbyNameAndPhoneAndEmail(dto);
-            }
+           if(!isNull(dto.getName()) && !isNull(dto.getMainPhoneNumber()) && !isNull(dto.getMainEmailAddress())) {
+               isValidation = true;
+           }
         }
         
-        if(!isNull(foundAccountIdbyCnpj)) {
-              HashMap<String, String> map = new HashMap<>();
-              map.put("PARAM_ID", String.valueOf(dto.getRowId()));
-              map.put("PROC_ACC_ID", foundAccountIdbyCnpj);
-              map.put("checkcu","update");
-              
-              mapper.insertProcessAccount(map);
-              mapper.insertReplicaAccount(map);
-
-              resulDto.setContactId(foundAccountIdbyCnpj);
-              resulDto.setErrorSpcCode("0"); 
-              resulDto.setErrorSpcMessage("OK");
-        }else {
-            HashMap<String, String> map = new HashMap<>();
-            map.put("PARAM_ID", String.valueOf(dto.getRowId()));
-            map.put("checkcu","insert");
+        if(isValidation) {
+            HashMap<String, String> map = new HashMap<String,String>();
+            map.put("PARAM_ID",    String.valueOf(dto.getRowId()));
+            map.put("PROC_ACC_ID", mapper.getAccountRowId(dto));
+            mapper.insertProcessAccount(map); //process.acount upsert
             
-            mapper.insertProcessAccount(map);  
+            map.put("PARAM_ID",    String.valueOf(dto.getRowId()));
+            map.put("PROC_ACC_ID", mapper.getAccountRowId(dto));
+            mapper.insertReplicaAccount(map); //replica.acount upsert
             
-            String foundAccountId = mapper.foundAccountId(dto);
-            
-            map.put("PROC_ACC_ID", foundAccountId );
-            mapper.insertReplicaAccount(map);
-            
-            resulDto.setContactId(foundAccountId);
-            resulDto.setErrorSpcCode("0"); 
+            resulDto.setErrorSpcCode("0");
             resulDto.setErrorSpcMessage("OK");
+        }else {
+            HashMap<String, String> map = new HashMap<String,String>();
+            
+            map.put("cnpjNumber", dto.getCnpjNumber());
+            map.put("name",dto.getName());
+            map.put("mainPhoneNumber",dto.getMainPhoneNumber());
+            map.put("mainEmailAddress",dto.getMainEmailAddress());
+            
+            resulDto.setErrorSpcCode("1");
+            resulDto.setErrorSpcMessage(JsonUtils.toJson(map));
         }
-
         return resulDto;
     }
     
