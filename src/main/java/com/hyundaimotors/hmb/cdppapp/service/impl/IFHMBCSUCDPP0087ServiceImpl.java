e25fac0903ee9fa23ac98be69f72ebcb5fb696a9
Jinhyeok Choi <jhchoi@anchors-biz.com>
2023-11-30 00:03:30 +0900

    search contact servicerequest Update

--- src/main/java/com/hyundaimotors/hmb/cdppapp/service/impl/IFHMBCSUCDPP0087ServiceImpl.java
+++ src/main/java/com/hyundaimotors/hmb/cdppapp/service/impl/IFHMBCSUCDPP0087ServiceImpl.java
@@ -11,6 +11,8 @@ import com.hyundaimotors.hmb.cdppapp.dto.IFHMBCSUCDPP0087.SearchAutoVehicleDto;
 import com.hyundaimotors.hmb.cdppapp.dto.IFHMBCSUCDPP0087.SearchCSISurveyDto;
 import com.hyundaimotors.hmb.cdppapp.dto.IFHMBCSUCDPP0087.SearchCampaignDto;
 import com.hyundaimotors.hmb.cdppapp.dto.IFHMBCSUCDPP0087.SearchContactOutputDto;
+import com.hyundaimotors.hmb.cdppapp.dto.IFHMBCSUCDPP0087.SearchServiceRequestContactDto;
+import com.hyundaimotors.hmb.cdppapp.dto.IFHMBSAPCDPP0031.SearchServiceRequestDto;
 import com.hyundaimotors.hmb.cdppapp.mapper.IFHMBCSUCDPP0087Mapper;
 import com.hyundaimotors.hmb.cdppapp.service.IFHMBCSUCDPP0087Service;
 
@@ -35,21 +37,27 @@ public class IFHMBCSUCDPP0087ServiceImpl implements IFHMBCSUCDPP0087Service{
                 listOfAutoVehicle = mapper.getVehicleList(dto);
                 
                 if(0 < listOfAutoVehicle.size()){
-                    listOfContactOutput.get(i).setListOfAutoVehicle(listOfAutoVehicle);
+                    listOfContactOutput.get(i).setAutoVehicle(listOfAutoVehicle);
                 }
 
                 List<SearchCampaignDto> listOfCampaign = new ArrayList<>();
                 listOfCampaign = mapper.getCampaignList(dto);
 
                 if(0 < listOfCampaign.size()){
-                    listOfContactOutput.get(i).setListOfCampaign(listOfCampaign);
+                    listOfContactOutput.get(i).setCampaign(listOfCampaign);
                 }
 
                 List<SearchCSISurveyDto> listOfCSISurvey = new ArrayList<>();
                 listOfCSISurvey = mapper.getCSISurveyList(dto);
 
                 if(0 < listOfCSISurvey.size()){
-                    listOfContactOutput.get(i).setListOfCSISurvey(listOfCSISurvey);
+                    listOfContactOutput.get(i).setCsiSurvey(listOfCSISurvey);
+                }
+
+                List<SearchServiceRequestContactDto> listOfSearchServiceRequest = new ArrayList<>();
+                listOfSearchServiceRequest = mapper.getServiceRequestList(dto);
+                if(0 < listOfSearchServiceRequest.size()){
+                    listOfContactOutput.get(i).setServiceRequest(listOfSearchServiceRequest);
                 }
             }
 
