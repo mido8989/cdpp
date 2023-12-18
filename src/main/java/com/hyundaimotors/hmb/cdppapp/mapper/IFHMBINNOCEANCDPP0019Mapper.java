99e26f72e532f388d7763d5c4c495d503d8665ac
jjeong100 <jjeoong100@naver.com>
2023-12-18 23:33:40 +0900

    수정

--- src/main/java/com/hyundaimotors/hmb/cdppapp/mapper/IFHMBINNOCEANCDPP0019Mapper.java
+++ src/main/java/com/hyundaimotors/hmb/cdppapp/mapper/IFHMBINNOCEANCDPP0019Mapper.java
@@ -11,4 +11,8 @@ public interface IFHMBINNOCEANCDPP0019Mapper {
     public List<String> getHolyDayList(IFHMBINNOCEANCDPP0019Dto dto)throws Exception;
 
     public List<String> getMaintenanceDayList(IFHMBINNOCEANCDPP0019Dto dto)throws Exception;
+    
+    //week sample
+    public List<String> getSampleWeekDay(IFHMBINNOCEANCDPP0019Dto dto)throws Exception;
+    
 }
