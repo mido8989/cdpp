3c632b7f6038bd161f610bcaae4b505812e24474
ChoiJinHwuk <jhchoi@anchors-biz.com>
2023-12-28 20:31:19 +0900

    Update Get Lead

--- src/main/java/com/hyundaimotors/hmb/cdppapp/service/IFHMBDMSCDPP0004Service.java
+++ src/main/java/com/hyundaimotors/hmb/cdppapp/service/IFHMBDMSCDPP0004Service.java
@@ -1,9 +1,16 @@
 package com.hyundaimotors.hmb.cdppapp.service;
 
+import java.util.HashMap;
+
+import org.springframework.scheduling.annotation.Async;
+
 import com.hyundaimotors.hmb.cdppapp.dto.IFHMBDMSCDPP0004.IFHMBDMSCDPP0004Dto;
 
 public interface IFHMBDMSCDPP0004Service {
     
     public IFHMBDMSCDPP0004Dto insertObject(IFHMBDMSCDPP0004Dto dto)throws Exception;
+
+    @Async
+    public void UpdateOppt(HashMap<String, String[]> map)throws Exception;
     
 }
