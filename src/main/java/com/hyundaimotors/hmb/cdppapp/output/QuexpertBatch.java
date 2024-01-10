89699e457c7c357d7dc729a771d301b402cee7cc
ChoiJinHwuk <jhchoi@anchors-biz.com>
2024-01-11 04:16:55 +0900

    Stop Get Lead Batch

--- src/main/java/com/hyundaimotors/hmb/cdppapp/output/QuexpertBatch.java
+++ src/main/java/com/hyundaimotors/hmb/cdppapp/output/QuexpertBatch.java
@@ -50,7 +50,7 @@ public class QuexpertBatch {
     ModelMapper modelMapper = new ModelMapper();
 
     
-    @Scheduled(cron = "0 */5 * * * *")
+    //@Scheduled(cron = "0 */5 * * * *")
     public void getQuExpert() throws ParseException{
         UUID IF_TR_ID = UUID.randomUUID();
 
