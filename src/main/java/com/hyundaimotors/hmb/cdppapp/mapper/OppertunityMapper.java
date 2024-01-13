52fe071cdff3aa74ad2a3de67f4c3bb8339d8156
ChoiJinHwuk <jhchoi@anchors-biz.com>
2024-01-13 10:00:49 +0900

    Opportunity Duplica Check

--- src/main/java/com/hyundaimotors/hmb/cdppapp/mapper/OppertunityMapper.java
+++ src/main/java/com/hyundaimotors/hmb/cdppapp/mapper/OppertunityMapper.java
@@ -3,6 +3,8 @@ package com.hyundaimotors.hmb.cdppapp.mapper;
 import com.hyundaimotors.hmb.cdppapp.dto.Oppertunity.OppertunityDto;
 
 public interface OppertunityMapper {
+
+    public int getRowId(OppertunityDto dto)throws Exception;
     
     public void insertObject(OppertunityDto dto)throws Exception;
 
